af0b508b0a65f5a88979dc3ff035a49e0c7389bd
Snow_city <3262827803@qq.com>
2025-01-07 18:31:05 +0800

    尝试出现UI（未果）

--- src/main/java/com/snowcity/snowcityswork/common/block/machine/electric/VoltaicPile.java
+++ src/main/java/com/snowcity/snowcityswork/common/block/machine/electric/VoltaicPile.java
@@ -4,9 +4,19 @@ import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
 import com.gregtechceu.gtceu.common.data.machines.GTMachineUtils;
 
+import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
+
 public class VoltaicPile extends SimpleTieredMachine{
     public VoltaicPile(IMachineBlockEntity holder, int tier, Object... args) {
         super(holder, tier, GTMachineUtils.defaultTankSizeFunction, args);
+    }
+
+    protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(VoltaicPile.class,
+            SimpleTieredMachine.MANAGED_FIELD_HOLDER);
+
+    @Override
+    public ManagedFieldHolder getFieldHolder() {
+        return MANAGED_FIELD_HOLDER;
     }
 
     @Override
